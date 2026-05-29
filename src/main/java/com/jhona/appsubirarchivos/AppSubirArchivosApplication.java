9fb344fcde4c8fc9353897343ac210c3cf21c984
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-28 21:09:12 -0500

    feat: init

---
+++ src/main/java/com/jhona/appsubirarchivos/AppSubirArchivosApplication.java
@@ -0,0 +1,13 @@
+package com.jhona.appsubirarchivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSubirArchivosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSubirArchivosApplication.class, args);
+    }
+
+}
